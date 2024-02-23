d0581365dd5a8fa95a7bdea9ea05dab1e4ea1a91
Amir.h Hooshmand <amir.hsmart@gmail.com>
2024-02-24 00:46:24 +0330

    update cloudflare hooks

--- manual_hooks/cf.auth.sh
+++ manual_hooks/cf.auth.sh
@@ -3,34 +3,34 @@
 # Get the path of the script's directory
 REPO_PATH=$(dirname "$(readlink -f "$0")")
 
-# Read the API key and zone ID from the creds.txt file
-API_KEY=$(grep API_KEY "$REPO_PATH"/creds.txt | cut -d '"' -f 2)
-ZONE_ID=$(grep ZONE_ID "$REPO_PATH"/creds.txt | cut -d '"' -f 2)
+# Read the API key and zone ID from the creds file
+API_KEY=$(grep API_KEY "$REPO_PATH/$CERTBOT_DOMAIN/cf.creds" | cut -d '"' -f 2)
+ZONE_ID=$(grep ZONE_ID "$REPO_PATH/$CERTBOT_DOMAIN/cf.creds" | cut -d '"' -f 2)
 
 # Create the domain for ACME challenge
 CREATE_DOMAIN="_acme-challenge.$CERTBOT_DOMAIN"
 
 # Create a DNS record using the Cloudflare API
 curl -s -X POST "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records" \
-     -H "Authorization: Bearer $API_KEY" \
-     -H "Content-Type: application/json" \
-     --data '{"type":"TXT","name":"'"$CREATE_DOMAIN"'","content":"'"$CERTBOT_VALIDATION"'","ttl":120}'
+    -H "Authorization: Bearer $API_KEY" \
+    -H "Content-Type: application/json" \
+    --data '{"type":"TXT","name":"'"$CREATE_DOMAIN"'","content":"'"$CERTBOT_VALIDATION"'","ttl":120}'
 
 # Retrieve the ID of the created DNS record
 RECORD_ID=$(curl -s "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records?type=TXT&name=$CREATE_DOMAIN" \
-                -H "Content-Type:application/json" \
-                -H "Authorization: Bearer $API_KEY" \
-                | grep -oE "\"id\":\"\w+\"" \
-                | cut -d '"' -f 4)
+    -H "Content-Type:application/json" \
+    -H "Authorization: Bearer $API_KEY" |
+    grep -oE "\"id\":\"\w+\"" |
+    cut -d '"' -f 4)
 
 # Create a directory for storing temporary files if it doesn't exist
-if [ ! -d "$REPO_PATH"/tmp/CERTBOT_"$CERTBOT_DOMAIN" ];then
-        mkdir -m 0700 "$REPO_PATH"/tmp/CERTBOT_"$CERTBOT_DOMAIN"
+if [ ! -d "$REPO_PATH/$CERTBOT_DOMAIN/CERTBOT" ]; then
+    mkdir -pm 0700 "$REPO_PATH/$CERTBOT_DOMAIN/CERTBOT"
 fi
 
 # Write the zone ID and record ID to temporary files
-echo "$ZONE_ID" > "$REPO_PATH"/tmp/CERTBOT_"$CERTBOT_DOMAIN"/ZONE_ID
-echo "$RECORD_ID" > "$REPO_PATH"/tmp/CERTBOT_"$CERTBOT_DOMAIN"/RECORD_ID
+echo "$ZONE_ID" >"$REPO_PATH/$CERTBOT_DOMAIN/CERTBOT/ZONE_ID"
+echo "$RECORD_ID" >"$REPO_PATH/$CERTBOT_DOMAIN/CERTBOT/RECORD_ID"
 
 # Wait for 15 seconds - this is the time it takes for the DNS record to be updated
 sleep 15
--- manual_hooks/cf.clean.sh
+++ manual_hooks/cf.clean.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 REPO_PATH=$(dirname "$(readlink -f "$0")")
-API_KEY=$(grep API_KEY "$REPO_PATH"/creds.txt | cut -d '"' -f 2)
+API_KEY=$(grep API_KEY "$REPO_PATH/$CERTBOT_DOMAIN/cf.creds" | cut -d '"' -f 2)
 
-if [ -f "$REPO_PATH"/tmp/CERTBOT_"$CERTBOT_DOMAIN"/ZONE_ID ]; then
-        ZONE_ID=$(cat "$REPO_PATH"/tmp/CERTBOT_"$CERTBOT_DOMAIN"/ZONE_ID)
-        rm -f "$REPO_PATH"/tmp/CERTBOT_"$CERTBOT_DOMAIN"/ZONE_ID
+if [ -f "$REPO_PATH/$CERTBOT_DOMAIN/CERTBOT/ZONE_ID" ]; then
+    ZONE_ID=$(cat "$REPO_PATH/$CERTBOT_DOMAIN/CERTBOT/ZONE_ID")
+    rm -f "$REPO_PATH/$CERTBOT_DOMAIN/CERTBOT/ZONE_ID"
 fi
 
-if [ -f "$REPO_PATH"/tmp/CERTBOT_"$CERTBOT_DOMAIN"/RECORD_ID ]; then
-        RECORD_IDS=$(cat "$REPO_PATH"/tmp/CERTBOT_"$CERTBOT_DOMAIN"/RECORD_ID)
-        rm -f "$REPO_PATH"/tmp/CERTBOT_"$CERTBOT_DOMAIN"/RECORD_ID
+if [ -f "$REPO_PATH/$CERTBOT_DOMAIN/CERTBOT/RECORD_ID" ]; then
+    RECORD_IDS=$(cat "$REPO_PATH/$CERTBOT_DOMAIN/CERTBOT/RECORD_ID")
+    rm -f "$REPO_PATH/$CERTBOT_DOMAIN/CERTBOT/RECORD_ID"
 fi
 
 # Remove the challenge TXT record from the zone
 if [ -n "${ZONE_ID}" ]; then
     if [ -n "${RECORD_IDS}" ]; then
-        for RECORD_ID in $RECORD_IDS
-        do curl -s -X DELETE "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records/$RECORD_ID" \
+        for RECORD_ID in $RECORD_IDS; do
+            curl -s -X DELETE "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records/$RECORD_ID" \
                 -H "Authorization: Bearer $API_KEY" \
                 -H "Content-Type: application/json"
         done
