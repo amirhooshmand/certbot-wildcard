155cb502d4700978e7b52a8f3dc5179226ac48e7
Amir.h Hooshmand <amir.hsmart@gmail.com>
2024-02-23 21:31:13 +0330

    add cloudflare hooks

---
+++ manual_hooks/cf.clean.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+REPO_PATH=$(dirname "$(readlink -f "$0")")
+API_KEY=$(grep API_KEY "$REPO_PATH"/creds.txt | cut -d '"' -f 2)
+
+if [ -f "$REPO_PATH"/tmp/CERTBOT_"$CERTBOT_DOMAIN"/ZONE_ID ]; then
+        ZONE_ID=$(cat "$REPO_PATH"/tmp/CERTBOT_"$CERTBOT_DOMAIN"/ZONE_ID)
+        rm -f "$REPO_PATH"/tmp/CERTBOT_"$CERTBOT_DOMAIN"/ZONE_ID
+fi
+
+if [ -f "$REPO_PATH"/tmp/CERTBOT_"$CERTBOT_DOMAIN"/RECORD_ID ]; then
+        RECORD_IDS=$(cat "$REPO_PATH"/tmp/CERTBOT_"$CERTBOT_DOMAIN"/RECORD_ID)
+        rm -f "$REPO_PATH"/tmp/CERTBOT_"$CERTBOT_DOMAIN"/RECORD_ID
+fi
+
+# Remove the challenge TXT record from the zone
+if [ -n "${ZONE_ID}" ]; then
+    if [ -n "${RECORD_IDS}" ]; then
+        for RECORD_ID in $RECORD_IDS
+        do curl -s -X DELETE "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records/$RECORD_ID" \
+                -H "Authorization: Bearer $API_KEY" \
+                -H "Content-Type: application/json"
+        done
+    fi
+fi
