2625e8342ffb73f46087d3f6e57df7b0a0853a20
Amir.h Hooshmand <amir.hsmart@gmail.com>
2024-02-24 22:55:49 +0330

    Add support for configuration file (.creds) for automated input

--- get-cert.sh
+++ get-cert.sh
@@ -3,7 +3,7 @@
 echo "This script will help you to obtain a free SSL certificate from Let's Encrypt."
 echo "This script is tested on Ubuntu 18.04 and 20.04."
 echo "If you are using a different OS, you may need to modify the script."
-read -p "Press Enter to continue or Ctrl+C to exit." tmp
+echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
 
 # Check if Certbot is installed
 if command -v certbot &>/dev/null; then
@@ -18,59 +18,78 @@ fi
 REPO_PATH=$(dirname "$(readlink -f "$0")")
 HOOK_DRECTORY="$REPO_PATH/manual_hooks"
 
-# Prompt the user to enter the domain
-read -p "Enter the domain: " DOMAIN_NAME
+if [ -f "$REPO_PATH/.creds" ]; then
+    # Get the credentials from the .creds file
+    echo "Reading credentials from $REPO_PATH/.creds"
+
+    DOMAIN=$(grep DOMAIN "$REPO_PATH/.creds" | cut -d '"' -f 2)
+    CONTACT=$(grep CONTACT "$REPO_PATH/.creds" | cut -d '"' -f 2)
+    CLOUD=$(grep CLOUD "$REPO_PATH/.creds" | cut -d '"' -f 2)
+    API_KEY=$(grep API_KEY "$REPO_PATH/.creds" | cut -d '"' -f 2)
+    ZONE_ID=$(grep ZONE_ID "$REPO_PATH/.creds" | cut -d '"' -f 2)
+
+    echo "Domain name: $DOMAIN" 
+fi
+
+if [ -z "$DOMAIN" ]; then
+    # Prompt the user to enter the domain
+    read -p "Enter the domain: " DOMAIN
+fi
 
 # Suggest a default email address
-DEFAULT_EMAIL="info@$DOMAIN_NAME"
+DEFAULT_EMAIL="info@$DOMAIN"
 
-# Prompt the user to enter the email
-read -p "Enter the email [$DEFAULT_EMAIL]: " CONTACT
+if [ -z "$CONTACT" ]; then
+    # Prompt the user to enter the email
+    read -p "Enter the email [$DEFAULT_EMAIL]: " CONTACT
+fi
 
-# Prompt the user to select the cloud provider
-echo "Select the cloud provider:"
-echo "1. Cloudflare"
-echo "2. ArvanCloud"
+if [ -z "$CLOUD" ]; then
+    # Prompt the user to select the cloud provider
+    echo "Select the cloud provider:"
+    echo "1. Cloudflare"
+    echo "2. ArvanCloud"
 
-mkdir -p "$HOOK_DRECTORY/$DOMAIN_NAME"
+    mkdir -p "$HOOK_DRECTORY/$DOMAIN"
 
-while true; do
-    read -p "Enter your choice (1 or 2): " CLOUD_CHOICE
-    case $CLOUD_CHOICE in
-    1)
-        CLOUD="cf"
+    while true; do
+        read -p "Enter your choice (1 or 2): " CLOUD_CHOICE
+        case $CLOUD_CHOICE in
+        1)
+            CLOUD="cf"
 
-        # Prompt the API key and zone ID
-        read -p "Enter your Cloudflare API key: " API_KEY
-        read -p "Enter your Cloudflare zone ID: " ZONE_ID
+            # Prompt the API key and zone ID
+            read -p "Enter your Cloudflare API key: " API_KEY
+            read -p "Enter your Cloudflare zone ID: " ZONE_ID
 
-        # Save the API key and zone ID to the creds file
-        echo "API_KEY=\"$API_KEY\"" >"$HOOK_DRECTORY/$DOMAIN_NAME/$CLOUD.creds"
-        echo "ZONE_ID=\"$ZONE_ID\"" >>"$HOOK_DRECTORY/$DOMAIN_NAME/$CLOUD.creds"
+            # Save the API key and zone ID to the creds file
+            echo "API_KEY=\"$API_KEY\"" >"$HOOK_DRECTORY/$DOMAIN/$CLOUD.creds"
+            echo "ZONE_ID=\"$ZONE_ID\"" >>"$HOOK_DRECTORY/$DOMAIN/$CLOUD.creds"
 
-        break
-        ;;
-    2)
-        CLOUD="arvan"
+            break
+            ;;
+        2)
+            CLOUD="arvan"
 
-        # Prompt the API key
-        read -p "Enter your ArvanCloud API key: " API_KEY
+            # Prompt the API key
+            read -p "Enter your ArvanCloud API key: " API_KEY
 
-        # Save the API key to the creds file
-        echo "API_KEY=\"$API_KEY\"" >"$HOOK_DRECTORY/$DOMAIN_NAME/$CLOUD.creds"
+            # Save the API key to the creds file
+            echo "API_KEY=\"$API_KEY\"" >"$HOOK_DRECTORY/$DOMAIN/$CLOUD.creds"
 
-        break
-        ;;
-    *)
-        echo "Invalid choice. Please enter 1 or 2."
-        ;;
-    esac
-done
+            break
+            ;;
+        *)
+            echo "Invalid choice. Please enter 1 or 2."
+            ;;
+        esac
+    done
+fi
 
 # Use the default email if no input provided
 CONTACT="${CONTACT:-$DEFAULT_EMAIL}"
 
-MANUAL_HOOKS_BASE_URL="https://raw.githubusercontent.com/amirhooshmand/cloud-certbot/main/manual_hooks"
+MANUAL_HOOKS_BASE_URL="https://raw.githubusercontent.com/amirhooshmand/certbot-wildcard/main/manual_hooks"
 
 download_file() {
     file_url=$1
@@ -104,6 +123,6 @@ certbot certonly -n --agree-tos --manual \
     --no-eff-email --preferred-challenges=dns \
     --manual-auth-hook "$HOOK_DRECTORY"/$CLOUD.auth.sh \
     --manual-cleanup-hook "$HOOK_DRECTORY"/$CLOUD.clean.sh \
-    -m "$CONTACT" -d "$DOMAIN_NAME",*."$DOMAIN_NAME"
+    -m "$CONTACT" -d "$DOMAIN",*."$DOMAIN"
 
 rm -r "$HOOK_DRECTORY"
